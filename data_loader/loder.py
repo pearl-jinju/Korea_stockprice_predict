94994c4db7cf922e62a5199c8a04834a1cd03f5a
j_pearl <y2kwlsw@gmail.com>
2022-12-12 01:55:36 +0900

    fix : 시장 구분(KOSPI, KOSDAQ) 추가

--- data_loader/loder.py
+++ data_loader/loder.py
@@ -5,15 +5,22 @@ import pandas as pd
 import numpy as np
 import datetime
 from tqdm import tqdm
+import pickle
 
 
-# data 조회일자 기준 x일 이전부터 현재까지==============
-# 영업일 기준 최근일자 호출함수
-def date_from_now(day:int=0):        
+# 영업일 기준 최근일자 호출함수(L= linked_date_type ex- %Y%m%d , S=seperate_date_type ex- %Y-%m-%d)
+# date_from_now() 는 오늘날짜 date_from_now(365)은 365일 전 날짜를 str형태로 출력한다.
+def date_from_now(day:int=0, type="L"):        
     edate_str = stock.get_nearest_business_day_in_a_week()
-    edate_dt = datetime.datetime.strptime(edate_str, '%Y%m%d')
-    sdate_dt = edate_dt-datetime.timedelta(days=day)
-    sdate_str = sdate_dt.strftime('%Y%m%d')
+    if type == "L":
+        edate_dt = datetime.datetime.strptime(edate_str, '%Y%m%d')
+        sdate_dt = edate_dt-datetime.timedelta(days=day)
+        sdate_str = sdate_dt.strftime('%Y%m%d')
+    elif type == "S":
+        edate_dt = datetime.datetime.strptime(edate_str, '%Y%m%d')
+        sdate_dt = edate_dt-datetime.timedelta(days=day)
+        edate_str = edate_dt.strftime('%Y-%m-%d')
+        sdate_str = sdate_dt.strftime('%Y-%m-%d')
     return sdate_str
 
 # 종목 기초정보 제공 함수
@@ -43,15 +50,29 @@ def get_stock_price_info(ticker:str, day:int=50000, market:str="ALL"):
     df_result['종목명'] = stock_name
     return df_result
 
-# 티커 호출
-tickers = stock.get_market_ticker_list(date_from_now(), market="ALL")
 
-result_df = pd.DataFrame()
-for ticker in tqdm(tickers):
-    temp_df = get_stock_price_info(ticker)
-    result_df = pd.concat([result_df,temp_df])
-    
-result_df.columns = temp_df.columns
 
-result_df.to_csv("../data/get_stock_price_info.csv",index=False, encoding='utf-8')
-     
+if __name__=='__main__':   
+    for market in ["KOSPI","KOSDAQ"]:    
+        result_df = pd.DataFrame()
+        # 티커 호출
+        tickers = stock.get_market_ticker_list(date_from_now(), market=market)
+        for ticker in tqdm(tickers[:1]):  # tickers[1270] 오류 있음  
+                temp_df = get_stock_price_info(ticker)
+                result_df = pd.concat([result_df,temp_df])
+        result_df.columns = temp_df.columns
+        result_df['market'] = market
+        
+        with open(f'../data/stock_price_data_all_period_{market}.pickle', 'wb') as f:
+            pickle.dump(result_df, f)
+
+    # 임시저장 파일 load
+    with open('../data/stock_price_data_all_period_KOSPI.pickle', 'rb') as f:
+        kospi_df = pickle.load(f)
+    with open('../data/stock_price_data_all_period_KOSDAQ.pickle', 'rb') as f:
+        kosdaq_df = pickle.load(f)
+        
+    all_market_df = pd.concat([kospi_df,kosdaq_df])
+    # 최종 파일 생성
+    with open(f'../data/stock_price_data_all_period_ALL.pickle', 'wb') as f:
+        pickle.dump(all_market_df, f)
