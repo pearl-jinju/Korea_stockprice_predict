5999b0adc0d7ea8f171ef9f58e80ffac55ad7c44
j_pearl <y2kwlsw@gmail.com>
2022-12-14 23:50:32 +0900

    feat : pytorch MLP 모델 적용

--- data_loader/model.py
+++ data_loader/model.py
@@ -13,6 +13,15 @@ from optuna.samplers import TPESampler
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 import params
+from sklearn.utils import shuffle
+from tqdm import tqdm
+import time
+
+
+# ANN module
+import torch
+from torch import nn, optim   
+import torch.nn.functional as F    
 
 
 data = params.DATA_SET
@@ -24,120 +33,182 @@ with open(data, 'rb') as f:
 
 
 X_dataset = vector_dataset.iloc[:,:params.ANALYSIS_DAY].reset_index(drop=True)
+# # 5일씩 묶어보자
+# for idx,i in enumerate(range(0,params.ANALYSIS_DAY,5)):
+#     X_dataset[f"new_{idx}"] = X_dataset[i] + X_dataset[i+1]+ X_dataset[i+2] +X_dataset[i+3] +X_dataset[i+4]
+# X_dataset = X_dataset.iloc[:,-9:].reset_index(drop=True)
+
 Y_dataset = vector_dataset.iloc[:,params.ANALYSIS_DAY*2:].reset_index(drop=True)
-
-scaler = MinMaxScaler()
-X_dataset = scaler.fit_transform(X_dataset)
-print(X_dataset)
-
+print(Y_dataset)
 # random_seed 설정
-SEED = 1234
+SEED = 1
 
 # Train Vaild Test split
-X_train, X_vaild, Y_train, Y_vaild = train_test_split(X_dataset, Y_dataset, test_size=0.2, random_state=SEED, shuffle=True)
-X_vaild, X_test, Y_vaild, Y_test = train_test_split(X_vaild, Y_vaild, test_size=0.5, random_state=SEED, shuffle=True)
+X_train, X_vaild, Y_train, Y_vaild = train_test_split(X_dataset, Y_dataset, test_size=0.1, random_state=SEED, shuffle=True)
 
-mae_vaild_ls = []
-mae_test_ls = []
-param_ls = []
-
-# =======================================================================================
-optuna == LGBMRegressor
-sampler = TPESampler(seed=10)
-def objective(trial):
-    param = {
-        'objective': 'regression', # 회귀
-        'verbose': 1,
-        'metric': 'mae',
-        'boosting': trial.suggest_categorical("boosting", ["gbdt", "dart", "goss"]),
-        'max_depth': trial.suggest_int('max_depth',3, 20),
-        'learning_rate': trial.suggest_float("learning_rate", 1e-9, 1e-2),
-        # 'n_estimators': trial.suggest_int('n_estimators', 500, 2000),
-        'subsample': trial.suggest_float('subsample', 0.1, 1),
-        'num_iterations' : trial.suggest_int('num_iterations', 1000, 1200),
-        'scale_pos_weight' : trial.suggest_float('scale_pos_weight',1.1,1.5)
-    }
-    model = LGBMRegressor(**param)
-    lgb_model = model.fit(X_train, Y_train.values.ravel(), eval_set=[(X_vaild, Y_vaild)], verbose=0)
-    mae = mean_absolute_error(Y_vaild, lgb_model.predict(X_vaild))
-    mae_test = mean_absolute_error(Y_test, lgb_model.predict(X_test))
-    param_ls.append(lgb_model.get_params)  
-    mae_vaild_ls.append(mae)
-    mae_test_ls.append(mae_test)
-    return mae_test
-
-study = optuna.create_study(direction='minimize', sampler=sampler)
-study.optimize(objective, n_trials=20)
+X_train = torch.Tensor(X_train.values)
+Y_train = torch.Tensor(Y_train.values)
+print(X_train.shape)
+print(Y_train.shape)
+X_vaild = torch.Tensor(X_vaild.values)
+Y_vaild = torch.Tensor(Y_vaild.values)
+print(X_vaild.shape)
+print(Y_vaild.shape)
+# X_test = torch.Tensor(X_test.values)
+# Y_test = torch.Tensor(Y_test.values)
+# print(X_test.shape)
+# print(Y_test.shape)
 
 
-print(study.best_params)
-final_lgb_model = LGBMRegressor(boosting =study.best_params['boosting'], max_depth= study.best_params['max_depth'], learning_rate= study.best_params['learning_rate'], subsample= study.best_params['subsample'],num_iterations=study.best_params['num_iterations'],scale_pos_weight=study.best_params['scale_pos_weight'])
-# final_lgb_model = LGBMRegressor(boosting =study.best_params['boosting'], max_depth= study.best_params['max_depth'], learning_rate= study.best_params['learning_rate'], n_estimators= study.best_params['n_estimators'], subsample= study.best_params['subsample'],num_iterations=study.best_params['num_iterations'],scale_pos_weight=study.best_params['scale_pos_weight'])
-final_lgb_model.fit(X_train, Y_train)
+# model Hyperparams
+batch_size = 15000
+hidden_dim = 64
+epochs = 50
+dropout = 0.5
 
-pred_vaild = final_lgb_model.predict(X_vaild)
-vaild_mae = mean_absolute_error(Y_vaild,pred_vaild)
-pred_test = final_lgb_model.predict(X_test)
-test_mae = mean_absolute_error(Y_test,pred_test)
+# device setting
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-print(vaild_mae, test_mae)
-# 모델 저장
-joblib.dump(final_lgb_model, "../data/lgbm_model.pkl") 
-# =======================================================================================
+print(device)
+
+# defining utility class
+# by defining this, you only have to write "for loop" to load minibatch data
+class DataLoader(object):
+    def __init__(self, x, y, batch_size=batch_size, shuffle=True):
+        self.x = x
+        self.y = y
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.start_idx = 0
+        self.data_size = x.shape[0]
+        if self.shuffle:
+            self.reset()
+    
+    def reset(self):
+        self.x, self.y = shuffle(self.x, self.y)
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.start_idx >= self.data_size:
+            if self.shuffle:
+                self.reset()
+            self.start_idx = 0
+            raise StopIteration
+    
+        batch_x = self.x[self.start_idx:self.start_idx+self.batch_size]
+        batch_y = self.y[self.start_idx:self.start_idx+self.batch_size]
+
+        batch_x = torch.tensor(batch_x, dtype=torch.float, device=device)
+        batch_y = torch.tensor(batch_y, dtype=torch.float, device=device)
+
+        self.start_idx += self.batch_size
+
+        return (batch_x,batch_y)
+
+#defining MLP model
+#generally out_dim is more than 1, but this model only allows 1.
+class MLP(nn.Module):
+    def __init__(self, in_dim, hidden_dim, out_dim=1):
+        super(MLP, self).__init__()
+        
+        assert out_dim==1, 'out_dim must be 1'
+        
+        self.in_dim = in_dim
+        self.hidden_dim = hidden_dim
+        self.out_dim = out_dim
+        self.linear1 = nn.Linear(self.in_dim, self.hidden_dim)
+        self.linear2 = nn.Linear(self.hidden_dim, 32)
+        self.linear3 = nn.Linear(32, 1)
+        self.drop2D = nn.Dropout(p=dropout, inplace=False)  
+        self.LeakyReLU = nn.LeakyReLU(0.1)
+    
+        
+    
+    def forward(self, x):
+        
+        x = self.LeakyReLU(self.linear1(x))
+        x = self.drop2D(x)
+        x = torch.sigmoid(self.linear2(x))
+        x = torch.tanh(self.linear3(x))
+        x = x.squeeze(1)
+        return x
 
 
-# # optuna == SVR  현재 미작동 코드 수정 필요
-# sampler = TPESampler(seed=10)
-# def objective(trial):
-#     param = {
-#         'C': trial.suggest_float('C', 0.3, 10.0),
-#         'degree': trial.suggest_int('degree', 2, 7),
-#         'gamma': trial.suggest_float('gamma', 1e-3, 0.01),
-#         'epsilon': trial.suggest_float('epsilon', 0.05, 0.5),
-#     }
-#     clf = SVR(**param)
-#     clf.fit(X_train, Y_train.values.ravel())
-#     mae = mean_absolute_error(Y_vaild, clf.predict(X_vaild))
-#     mae_test = mean_absolute_error(Y_test, clf.predict(X_test))
-#     param_ls.append(lgb_model.get_params)  
-#     mae_vaild_ls.append(mae)
-#     mae_test_ls.append(mae_test)
-#     return mae_test
+#instantiate model
+mlp = MLP(X_train.shape[1], hidden_dim, 1).to(device)
+# Adam -->SGD
+optimizer = optim.SGD(mlp.parameters(), lr=0.001, weight_decay=0.00001,momentum=0.9,nesterov=True)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=5, eta_min=0.0001)
 
-# study = optuna.create_study(direction='minimize', sampler=sampler)
-# study.optimize(objective, n_trials=1)
-
-# print(study.best_params)
-
-# final_svr_model = SVR(C =study.best_params['C'], degree= study.best_params['degree'], gamma= study.best_params['gamma'], epsilon= study.best_params['epsilon'])
-# final_svr_model.fit(X_train, Y_train.values.ravel())
-
-# pred_vaild = final_svr_model.predict(X_vaild)
-# vaild_mae = mean_absolute_error(Y_vaild,pred_vaild)
-# pred_test = final_svr_model.predict(X_test)
-# test_mae = mean_absolute_error(Y_test,pred_test)
-
-# print(vaild_mae, test_mae)
-# # 모델 저장
-# joblib.dump(final_svr_model, "../data/final_svr_model.pkl") 
+train_dataloader = DataLoader(X_train, Y_train, batch_size=batch_size)
+valid_dataloader = DataLoader(X_vaild, Y_vaild, batch_size=batch_size)  
 
 
-# plt.plot(range(20),mae_vaild_ls, color='b')
-# plt.plot(range(20),mae_test_ls, marker='+', color='r')
-# plt.title('vaild_test', fontsize=20) 
-# plt.ylabel('mae', fontsize=14)
-# plt.xlabel('n_trials', fontsize=14)
-# plt.show()
+#this model learns to minimize MAE
+def mae_loss(y_pred, y_true):
+    mae = torch.abs(y_true - y_pred).mean()
+    return mae
+# create a function (this my favorite choice)
+def RMSELoss(yhat,y):
+    rmse = torch.sqrt(torch.mean((yhat-y)**2))
+    return rmse
 
-# params_df = pd.DataFrame(param_ls)
-# params_df.to_csv('Best_params.csv', index= False)
 
-# plot_importance 시각화
-# fm.get_fontconfig_fonts()
-# FONT_LOCATION = r'C:\Windows\Fonts/malgun.ttf'
-# font_name = fm.FontProperties(fname=FONT_LOCATION).get_name()
-# plt.rc('font', family=font_name)
-# print(plt.rcParams['font.family'])
-# fig, ax = plt.subplots(figsize=(10,12))
-# plot_importance(lgbm_wrapper, ax=ax)
-# plt.show()
+
+
+#to plot loss curve after training
+valid_losses = []
+
+for epoch in range(epochs):
+    start_time = time.time()
+    mlp.train()
+    num_batch = train_dataloader.data_size // train_dataloader.batch_size + 1
+    
+    for batch_id, (batch_x, batch_y) in enumerate(train_dataloader):
+        
+        y_pred = mlp(batch_x)
+
+        loss = mae_loss(y_pred, batch_y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        elapsed_time = time.time() - start_time
+        elapsed_min = int(elapsed_time / 60)
+        elapsed_sec = elapsed_time - 60 * elapsed_min
+
+        print('\rEpoch:{} Batch:{}/{} Loss:{:.4f} Time:{}m{:.2f}s'.format(epoch + 1, batch_id, 
+                                                                          num_batch, loss.item(),
+                                                                          elapsed_min, elapsed_sec), end='')
+    scheduler.step()
+    print()
+    mlp.eval()
+    valid_loss = 0
+    best_loss = np.inf
+    num_batch = valid_dataloader.data_size // valid_dataloader.batch_size + 1
+    
+    for batch_id, (batch_x, batch_y) in enumerate(valid_dataloader):
+    
+        y_pred = mlp(batch_x)
+        loss = mae_loss(y_pred, batch_y)
+        valid_loss += loss.item()
+    
+    valid_loss /= num_batch
+    valid_losses.append(valid_loss)
+    
+    #save model when validation loss is minimum
+    if valid_loss < best_loss:
+        best_loss = valid_loss
+        torch.save(mlp.state_dict(), f'../model/mlp_model_batch_{batch_size}_best_{best_loss:.6f}.model')  
+    
+    print('Valid Loss:{:.4f}'.format(valid_loss))
+    
+
+#plot validation loss curve, this may help to notice overfitting
+plt.figure(figsize=(16,5))
+plt.ylim(0,max(valid_losses)+0.02)
+plt.plot(valid_losses)
+print('minimum validation loss is {:.4f}'.format(min(valid_losses)))
