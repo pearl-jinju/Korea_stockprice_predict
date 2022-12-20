91e3dc574e15973f41b43b1b3fa7b1331f3802c2
j_pearl <y2kwlsw@gmail.com>
2022-12-20 23:45:03 +0900

    feat: 종목 기준 테마조회 및 테마 내 세부종목 조회 기능 추가

--- data_loader/loader.py
+++ data_loader/loader.py
@@ -2,8 +2,15 @@ from pykrx import stock
 import pandas as pd
 import datetime
 from tqdm import tqdm
+import time
+import requests
+from bs4 import BeautifulSoup
+import numpy as np
+import re
 import pickle
 import os
+
+
 
 
 def date_from_now(day:int=0, type="L"):
@@ -24,28 +31,33 @@ def date_from_now(day:int=0, type="L"):
     return sdate_str
 
 
-def get_stock_basic_info(day:int=0, market:str="ALL"):
+def get_stock_basic_info(day=0, market="ALL", detail="ALL"):
     """ 종목 기초정보 제공 함수
         티커/종목명/시가/종가/변동폭(오늘)/등락률(오늘)/\n
         거래량(오늘)/거래대금(오늘)/상장주식수/보유수량/\n
         지분율/한도수량/한도소진률/BPS/PER/PBR/EPS/DIV/DPS_
+        
+        All = 모든 정보
+        BASIC = 기초 정보
 
     Args:
         day (int, optional): _description_. Defaults to 0.
         market (str, optional): _description_. Defaults to "ALL".
+        detail (str, optional): _description_. Defaults to "ALL".
 
     Returns:
         _type_: DataFrame
     """
-    df_stake_info = stock.get_exhaustion_rates_of_foreign_investment(date_from_now(), market).reset_index()
-    df_change = stock.get_market_price_change(date_from_now(day),date_from_now(), market=market).reset_index()
-    df_result = pd.merge(df_change,df_stake_info, on='티커',how='left')
-    df_fundamental = stock.get_market_fundamental( date_from_now(), market=market).reset_index()
-    df_result = pd.merge(df_result,df_fundamental, on='티커',how='left')
-    return df_result
-
-df_stock_basic_info = get_stock_basic_info()
-
+    if detail=="ALL":
+        df_stake_info = stock.get_exhaustion_rates_of_foreign_investment(date_from_now(), market).reset_index()
+        df_change = stock.get_market_price_change(date_from_now(day),date_from_now(), market=market).reset_index()
+        df_result = pd.merge(df_change,df_stake_info, on='티커',how='left')
+        df_fundamental = stock.get_market_fundamental( date_from_now(), market=market).reset_index()
+        df_result = pd.merge(df_result,df_fundamental, on='티커',how='left')
+        return df_result
+    if detail=="BASIC":
+        df_change = stock.get_market_price_change(date_from_now(day),date_from_now(), market=market).reset_index()
+        return df_change
 
 
 
@@ -55,6 +67,7 @@ def get_stock_price_info(ticker, market="ALL", info="ALL", day=50000):
     info="ALL" 거래량과 펀더멘탈 정보 포함
     info="BASIC" 기본 정보만 포함
     """
+    df_stock_basic_info = get_stock_basic_info()
     df_stock_ohlcv = stock.get_market_ohlcv(date_from_now(day), date_from_now(), ticker)
     cond = df_stock_basic_info['티커']==ticker
     stock_name = df_stock_basic_info[cond]['종목명'].values[0]
@@ -155,8 +168,99 @@ def get_index_fundamental_info(stock_ticker):
                'DIV':market_div}
         return res
 
+def get_thema():
+    #크롤링 차단 막기
+    seed = np.random.randint(100)
+    np.random.seed(seed)
+    delay = np.random.randint(5)/100
+    
+    theama_idx=1
+    idx_max = 750
+    stock_idx_max = 100
+
+    result_df = pd.DataFrame(columns=['테마명',"종목명", "테마 사유"])
+    while theama_idx < idx_max:
+        # 테마 조회
+        search_url = f'https://finance.naver.com/sise/sise_group_detail.naver?type=theme&no={theama_idx}'
+        resp = requests.get(search_url)
+        soup = BeautifulSoup(resp.text, 'html.parser')
+        s = soup.select_one
+        time.sleep(delay)
+
+        try:
+            thema_name = s('head > title').text.split(':')[0] # re.sub(r"/", "_", s('head > title').text.split(':')[0])
+            theama_idx+=1
+            stock_idx=1
+            
+            while stock_idx<stock_idx_max:
+                try:
+                    stock_name = re.sub(r"&",r"&",re.sub(r"\s", "", re.sub(r"\t", "",s(f'#contentarea > div:nth-child(5) > table > tbody > tr:nth-child({stock_idx}) > td:nth-child(2) > div > div > strong').text)).split('\n')[0])
+                    # print(stock_name)
+                    stock_detail = re.sub(r"\n\n", "", re.sub(r"\t", "",s(f'#contentarea > div:nth-child(5) > table > tbody > tr:nth-child({stock_idx}) > td:nth-child(2) > div > div > p').text))
+                    data = pd.DataFrame([[thema_name,stock_name, stock_detail]])
+                    data.columns = ['테마명',"종목명", "테마 사유"]
+                    stock_idx+=1
+                    result_df = pd.concat([result_df, data])
+                except AttributeError:
+                    break
+
+        except AttributeError:
+            print(f"pass{theama_idx}/{idx_max}")
+            theama_idx+=1
+            continue
+        
+    result_df = result_df.reset_index(drop=True)
+        
+    # save
+    with open('../data/thema_data.pkl', 'wb') as f:
+        pickle.dump(result_df, f)
+        
+        
+def get_included_thema_stocks_in_thema(name:str):
+    """종목명을 입력하면,\n
+       그 종목이 포함된 테마종류와,\n
+       테마 내에 속한 종목들의 테마사유, 종가, 등락률을 반환합니다.
+
+    Args:
+        name (str): _description_
+    """
+    stock_name = name
+
+    with open('../data/thema_data.pkl', 'rb') as f:
+        df = pickle.load(f)
+    #종목 기초정보
+    df_change = get_stock_basic_info(day=0, market="ALL", detail="BASIC")
+    df_change = df_change.iloc[:,1:6]
+    del df_change['시가']
+    del df_change['변동폭']
+        
+    # 인덱스 재정렬    
+    df = df.reset_index(drop=True)
+    # 입력된 종목명으로 필터링
+    cond = df['종목명']==stock_name
+    # 종목이 포함된 테마 출력
+    thema_df = df[cond]
+    included_thema =  thema_df['테마명'].values
+    
+    stocks_in_thema_result =pd.DataFrame()
+    for thema in included_thema:
+        cond = df['테마명']==thema
+        stocks_in_thema = df[cond]
+        stocks_in_thema = pd.merge(stocks_in_thema,df_change, on="종목명", how='left')
+        stocks_in_thema = stocks_in_thema.sort_values(by='등락률', ascending=False)
+        stocks_in_thema_result = pd.concat([stocks_in_thema_result,stocks_in_thema])
+        
+    stocks_in_thema_result.columns = stocks_in_thema.columns
+    stocks_in_thema_result = stocks_in_thema_result[['테마명','종목명','종가','등락률','테마 사유']]
+
+
+    return [included_thema, stocks_in_thema_result]
+
 
 if __name__=='__main__':   
+    # 테마 정보 받아오기
+    get_thema()
+    
     for market in ["KOSPI","KOSDAQ"]:    
         result_df = pd.DataFrame()
         # 티커 호출
@@ -180,3 +284,4 @@ if __name__=='__main__':
     # 최종 파일 생성
     with open(f'../data/stock_price_data_all_period_ALL_2.pkl', 'wb') as f:
         pickle.dump(all_market_df, f)
+        
